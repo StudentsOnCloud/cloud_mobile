2e3f719e5914d6a56ce7418b99b918e1b1690133
egementrk <turk.egemen@hotmail.com>
2021-10-23 19:37:53 +0300

    first commit

---
+++ CloudMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloudMobile
+//
+//  Created by Egemen on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
